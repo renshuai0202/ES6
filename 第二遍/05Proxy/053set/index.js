784ab5d98a27423d618c65ef9fdc5adfcfbfa246
帅帅 <renshuai0202@gmail.com>
2023-05-09 15:46:02 +0800

    update: Proxy set

--- 第二遍/05Proxy/053set/index.js
+++ 第二遍/05Proxy/053set/index.js
@@ -10,10 +10,13 @@ const handler = {
 };
 
 const proxy = new Proxy(person, handler);
-console.log(proxy.name);
 
 proxy.name = 'lisi'
+console.log(proxy.name); // lisi
 
+person.name = 'wangwu';
+console.log(person.name); // wangwu
+console.log(proxy.name); // wangwu
 
 /**
  * set(target, propKey, propVal, proxyInstance)
@@ -21,4 +24,7 @@ proxy.name = 'lisi'
  *  propKey：属性名
  *  propVal：属性值
  *  proxyInstance 实例本身（可选）：严格地说，是操作行为所针对的对象
+ * 
+ *  经过proxy修改属性，会触发set
+ *  直接在person修改属性，不会触发set
  */
